71828be9588e41ba6d9ec8fa9255c93a149cfef8
satishvatturu <93177666+satishvatturu@users.noreply.github.com>
2023-10-13 16:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
-
-alert(numberOfBooksRead());
+    {
+      author: "Bill Gates",
+      title: "The Road Ahead",
+      readingStatus: true,
+    },
+    {
+      author: "Steve Jobs",
+      title: "Walter Isaacson",
+      readingStatus: true,
+    },
+    {
+      author: "Suzanne Collins",
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      readingStatus: false,
+    },
+  ];
+  
+  const numberOfBooksRead = () => {
+    // write your code here
+      let count = 0;
+      for(let i=0;i<library.length;i++)
+      {
+        if(library[i].readingStatus==true)
+        {
+            count++;
+        }
+      }
+      return count;
+  };
+  
+  // Do not change the code below
+  
+  alert(numberOfBooksRead());
+  
